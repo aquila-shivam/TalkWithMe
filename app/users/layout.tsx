634b4cfc09c37cd17dcc0a190e4172fdfd0e4c5c
aquila-shivam <72098611+aquila-shivam@users.noreply.github.com>
2023-07-28 10:18:23 +0530

    conversation

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,8 +1,15 @@
 import Sidebar from '@/app/components/sidebar/Sidebar'
-const layout = async ({children}:{children : React.ReactNode}) => {
+import getUsers from '../action/getUsers';
+import UserList from './components/UserList';
+const layout = async ({children}:
+  {children : React.ReactNode}) => {
+
+    const users = await getUsers();
+
   return ( 
     <Sidebar>
      <div className="h-full">
+      <UserList items={users}/>
       {children}
      </div>
     </Sidebar>
