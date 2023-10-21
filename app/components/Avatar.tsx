17774ba0a8ba1ce81f768c6b4ae54929a1bc4b2f
aquila-shivam <72098611+aquila-shivam@users.noreply.github.com>
2023-10-21 16:02:47 +0530

    finished

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -15,6 +15,7 @@ const Avatar:React.FC<AvatarProps> = ({
 
   const {members} = useActiveList();
   const isActive = members.indexOf(user?.email!) !== -1
+  console.log(isActive);
   return (
     <div className='relative'>
       <div className='
--- app/hooks/useActiveChannel.ts
+++ app/hooks/useActiveChannel.ts
@@ -1,41 +1,42 @@
-import useActiveList from "./useActiveList";
-import { Channel, Members } from "pusher-js";
 import { useEffect, useState } from "react";
 import { pusherClient } from "../libs/pusher";
+import { Channel, Members } from "pusher-js";
+import useActiveList from "./useActiveList";
 
-const useActiveChannel = () =>{
-    const {set,add,remove} = useActiveList();
-    const [activeChannel,setActiveChannel] = useState<Channel | null>(null); 
+const useActiveChannel = () => {
+  const { set, add, remove } = useActiveList();
+  const [activeChannel, setActiveChannel] = useState<Channel | null>(null);
 
-    useEffect(()=>{
-        let channel =  activeChannel;
-        if(!channel){
-            channel = pusherClient.subscribe('presence-messenger');
-            setActiveChannel(channel);
-        }
+  useEffect(() => {
+    let channel = activeChannel;
 
-        channel.bind('pusher:subscription_succeeded',(members:Members)=>{
-            const initialMembers : string[] = [];
+    if (!channel) {
+      channel = pusherClient.subscribe('presence-messenger');
+      setActiveChannel(channel);
+    }
 
-            members.each((member:Record<string,any>) => initialMembers.push(member.id));
-            set(initialMembers);
-        });
+    channel.bind("pusher:subscription_succeeded", (members: Members) => {
+      const initialMembers: string[] = [];
 
-        channel.bind("pusher:member_added",(member : Record<string,any>) =>{
-            add(member.id);
-        });
+      members.each((member: Record<string, any>) => initialMembers.push(member.id));
+      set(initialMembers);
+    });
 
-        channel.bind("pusher:member_removed",(member : Record<string,any>)=>{
-            remove(member.id);
-        })
+    channel.bind("pusher:member_added", (member: Record<string, any>) => {
+      add(member.id)
+    });
 
-        return ()=>{
-            if(activeChannel){
-                pusherClient.unsubscribe('presence-messenger');
-                setActiveChannel(null);
-            }
-        }
-    },[activeChannel,set,add,remove])
+    channel.bind("pusher:member_removed", (member: Record<string, any>) => {
+      remove(member.id);
+    });
+
+    return () => {
+      if (activeChannel) {
+        pusherClient.unsubscribe('presence-messenger');
+        setActiveChannel(null);
+      }
+    }
+  }, [activeChannel, set, add, remove]);
 }
 
 export default useActiveChannel;
