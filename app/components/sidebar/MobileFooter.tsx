bbad05fba5edd2ac7abc452bd023199583f3c809
aquila-shivam <72098611+aquila-shivam@users.noreply.github.com>
2023-10-23 01:44:01 +0530

    profile

--- app/components/sidebar/MobileFooter.tsx
+++ app/components/sidebar/MobileFooter.tsx
@@ -3,42 +3,73 @@
 import useConversation from "@/app/hooks/useConversation";
 import useRoutes from "@/app/hooks/useRoutes"
 import MobileItem from "./MobileItem";
+import Avatar from "../Avatar";
+import { User } from "@prisma/client";
+import SettingsModal from "./SettingsModal";
+import { useState } from "react";
 
-const MobileFooter = () => {
- 
- const routes = useRoutes();
- const {isOpen} = useConversation();
- 
- if(isOpen){
+interface MobileFooterProps {
+  currentUser: User
+}
+
+const MobileFooter: React.FC<MobileFooterProps> = ({
+  currentUser
+}) => {
+
+  const routes = useRoutes();
+  const { isOpen } = useConversation();
+  const [isOpenSetting, SetIsOpenSetting] = useState(false);
+
+  if (isOpen) {
     return null;
- }
+  }
 
 
- return (
-    <div
-    className="
+  return (
+    <>
+      <SettingsModal
+        currentUser={currentUser}
+        isOpen={isOpenSetting}
+        onClose={() => SetIsOpenSetting(false)}
+      />
+
+      <div
+        className="
         fixed
-        justify-between
         w-full
         bottom-0
         z-40
         flex
+        justify-evenly
         items-center
         bg-white
         border-t-[1px]
         lg:hidden
     "
-    >
-      {routes.map((route)=>(
-        <MobileItem
-        key={route.href}
-        href={route.href}
-        active={route.active}
-        icon ={route.icon}
-        onClick = {route.onClick}
-        />
-      ))}
-    </div>
+      >
+        {routes.map((route) => (
+          <MobileItem
+            key={route.href}
+            href={route.href}
+            active={route.active}
+            icon={route.icon}
+            onClick={route.onClick}
+          />
+        ))}
+
+        <div onClick={() => SetIsOpenSetting(true)}
+          className="
+            cursor-pointer
+            hover:opacity-75
+            transition
+            mr-5
+            mt-1
+        "
+        >
+          <Avatar user={currentUser} />
+        </div>
+      </div>
+    </>
   )
 }
 
